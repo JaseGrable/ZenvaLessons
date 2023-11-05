ed078b004c35252026e41150b9c78e65c9a75df7
JaseGrable <jase.grable@gmail.com>
2023-11-05 13:45:40 -0800

    Finished game code

--- PyGame/game.py
+++ PyGame/game.py
@@ -1,92 +1,104 @@
 import pygame
 from gameObject import GameObject
 from player import Player
-# Importing the Enemy class
 from enemy import Enemy
 
 
 class Game:
-
     def __init__(self):
-        self.width = 600
-        self.height = 600
+        self.width = 800
+        self.height = 800
         self.white_colour = (255, 255, 255)
-
         self.game_window = pygame.display.set_mode((self.width, self.height))
-
-        # Loading the game assets
-        # NOTE: We're using different values and image paths here specifically for the Trinket application
+        self.clock = pygame.time.Clock()
         self.background = GameObject(
             0, 0, self.width, self.height, 'assets/background.png')
-        self.treasure = GameObject(280, 35, 40, 40, 'assets/treasure.png')
-        self.player = Player(280, 530, 40, 40, 'assets/player.png', 1)
+        self.treasure = GameObject(375, 50, 50, 50, 'assets/treasure.png')
+        self.level = 1.0
+        self.reset_map()
 
-        self.enemy = Enemy(0, 450, 40, 40, 'assets/enemy.png', 5)
-
-        self.clock = pygame.time.Clock()
+    def reset_map(self):
+        self.player = Player(375, 700, 50, 50, 'assets/player.png', 10)
+        speed = 5 + (self.level * 5)
+        if self.level >= 4.0:
+            self.enemies = [
+                Enemy(0, 600, 50, 50, 'assets/enemy.png', speed),
+                Enemy(750, 400, 50, 50, 'assets/enemy.png', speed),
+                Enemy(0, 200, 50, 50, 'assets/enemy.png', speed),
+            ]
+        elif self.level >= 2.0:
+            self.enemies = [
+                Enemy(0, 600, 50, 50, 'assets/enemy.png', speed),
+                Enemy(750, 400, 50, 50, 'assets/enemy.png', speed),
+            ]
+        else:
+            self.enemies = [
+                Enemy(0, 600, 50, 50, 'assets/enemy.png', speed),
+            ]
 
     def draw_objects(self):
         self.game_window.fill(self.white_colour)
-
-        # Blitting assets to the screen
         self.game_window.blit(self.background.image,
                               (self.background.x, self.background.y))
         self.game_window.blit(self.treasure.image,
                               (self.treasure.x, self.treasure.y))
         self.game_window.blit(
             self.player.image, (self.player.x, self.player.y))
-
-        self.game_window.blit(self.enemy.image, (self.enemy.x, self.enemy.y))
-
+        for enemy in self.enemies:
+            self.game_window.blit(enemy.image, (enemy.x, enemy.y))
         pygame.display.update()
 
-    def detect_collision(self, object_1, object_2):
-        if object_1.y > (object_2.y + object_2.height):
-            return False
-        elif (object_1.y + object_1.height) < object_2.y:
-            return False
+    def move_objects(self, player_direction):
+        self.player.move(player_direction, self.height)
+        for enemy in self.enemies:
+            enemy.move(self.width)
 
-        if object_1.x > (object_2.x + object_2.width):
-            return False
-        elif (object_1.x + object_1.width) < object_2.x:
-            return False
-        return True
+    def check_if_collided(self):
+        for enemy in self.enemies:
+            if self.detect_collision(self.player, enemy):
+                self.level = 1.0
+                return True
+        if self.detect_collision(self.player, self.treasure):
+            self.level += 0.5
+            return True
+        return False
+
+    def detect_collision(self, object_1, object_2):
+        # if object_1.y > (object_2.y + object_2.height):
+        #     return False
+        # elif (object_1.y + object_1.height) < object_2.y:
+        #     return False
+        # if object_1.x > (object_2.x + object_2.width):
+        #     return False
+        # elif (object_1.x + object_1.width) < object_2.x:
+        #     return False
+        # return True
+        if object_1.y < (object_2.y + object_2.height) and (object_1.y + object_1.height) > object_2.y and object_1.x < (object_2.x + object_2.width) and (object_1.x + object_1.width) > object_2.x:
+            return True
+        return False
 
     def run_game_loop(self):
         player_direction = 0
-
         while True:
             # Handle events
             events = pygame.event.get()
             for event in events:
-                # If there's a QUIT event, we break the loop and exit the method
                 if event.type == pygame.QUIT:
                     return
-
-                # Listening for when a key is pressed down on the keyboard
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_UP:
                         player_direction = -1
                     elif event.key == pygame.K_DOWN:
                         player_direction = 1
-
-                # Stopping the player when arrow keys are released
                 elif event.type == pygame.KEYUP:
                     if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                         player_direction = 0
 
             # Execute logic
-            self.player.move(player_direction, self.height)
-            # Moving the enemy
-            self.enemy.move(self.width)
-
+            self.move_objects(player_direction)
             # Update display
             self.draw_objects()
-
-            # Detect Collisions
-            if self.detect_collision(self.player, self.enemy):
-                return
-            elif self.detect_collision(self.player, self.treasure):
-                return
-
+            # Detect collisions
+            if self.check_if_collided():
+                self.reset_map()
             self.clock.tick(60)
